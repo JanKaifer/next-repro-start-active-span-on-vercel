cfdbec82d4c6043d24a7ff6927203dd77e243279
Jan Kaifer <jan@kaifer.cz>
2023-03-09 11:48:28 +0100

    also try creating custom span

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getTracer } from "next/dist/server/lib/trace/tracer";
 import { NextServerSpan } from "next/dist/server/lib/trace/constants";
+import { trace } from "@opentelemetry/api";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  return await getTracer().trace(
-    NextServerSpan.getRequestHandler,
-    async (span) => {
-      res.status(200).send("Hello World");
-    }
+  await getTracer().trace(NextServerSpan.getRequestHandler, async (span) =>
+    sleep(1000)
   );
+
+  const span = trace.getTracer("default").startSpan("test");
+  await sleep(1000);
+  span.end();
+
+  res.status(200).send("Hello World");
 };
